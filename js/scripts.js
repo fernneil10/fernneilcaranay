d9b93043b2ffd18b778f5d14897ac2bcbd33f126
fernneil10 <38779988+fernneil10@users.noreply.github.com>
2018-07-05 22:31:10 +0800

    Update scripts.js
    added new scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
  jQuery(document).ready(function($) {
-
-     jQuery('#fullpage').fullpage({
+  
+  jQuery(window).load(function() {
+  
+ jQuery('#fullpage').fullpage({
          anchors: ['Home', 'AboutMe', 'MySkills', 'Portfolio', 'Contact'],
          menu: '#menu',
          navigation: true,
@@ -10,71 +12,86 @@
          scrollBar: false,
          css3:false
      });
-
-
-    equalheight = function(container){
-
-var currentTallest = 0,
-     currentRowStart = 0,
-     rowDivs = new Array(),
-     $el,
-     topPosition = 0;
- $(container).each(function() {
-
-   $el = $(this);
-   $($el).height('auto')
-   topPostion = $el.position().top;
-
-   if (currentRowStart != topPostion) {
-     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-       rowDivs[currentDiv].height(currentTallest);
-     }
-     rowDivs.length = 0; // empty the array
-     currentRowStart = topPostion;
-     currentTallest = $el.height();
-     rowDivs.push($el);
-   } else {
-     rowDivs.push($el);
-     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-  }
-   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-     rowDivs[currentDiv].height(currentTallest);
-   }
- });
-}
-
-$(window).load(function() {
-  equalheight('.eq-blocks');
+ 
 });
 
 
-$(window).resize(function(){
-  equalheight('.eq-blocks');
+jQuery(window).resize(function(){
+ 
+ jQuery('#fullpage').fullpage({
+         anchors: ['Home', 'AboutMe', 'MySkills', 'Portfolio', 'Contact'],
+         menu: '#menu',
+         navigation: true,
+         autoScrolling: true,
+         parallax: true,
+         parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+         scrollBar: false,
+         css3:false
+     });
+ 
 });
+  
+  jQuery('.portfolio-slider').slick({
+         centerMode: true,
+         centerPadding: '250px',
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         autoplay: true,
+         autoplaySpeed: 5000,
+         arrows: false,
+         responsive: [
+             {
+                 breakpoint: 1281,
+                 settings: {
+                     arrows: false,
+                     centerMode: true,
+                     centerPadding: '230px',
+                     slidesToShow: 1
+                 }
+             },
+             {
+                 breakpoint: 1025,
+                 settings: {
+                     arrows: false,
+                     centerMode: true,
+                     centerPadding: '140px',
+                     slidesToShow: 1
+                 }
+             },
+             {
+                 breakpoint: 992,
+                 settings: {
+                     arrows: false,
+                     centerMode: true,
+                     centerPadding: '40px',
+                     slidesToShow: 1
+                 }
+             },
+             {
+                 breakpoint: 481,
+                 settings: {
+                     arrows: false,
+                     centerMode: true,
+                     centerPadding: '15px',
+                     slidesToShow: 1
+                 }
+             },
+             {
+                 breakpoint: 361,
+                 settings: {
+                     arrows: false,
+                     centerMode: true,
+                     centerPadding: '0px',
+                     slidesToShow: 1
+                 }
+             }
+
+         ]
+     });
+
+     
 
 
-  // fc slider
-  jQuery('.testimonials-slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 1000,
-    // fade: true,
-    // cssEase: 'linear',
-    arrows: true,
-    slidesToShow: 2,
-    autoplay: true,
-      autoplaySpeed: 5000,
-      responsive: [
-
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-    ]
-  });
 
      jQuery('.burger-menu').click(function() {
          jQuery('.header-fix-menu').toggleClass('open-menu');
@@ -82,6 +99,3 @@ $(window).resize(function(){
 
  });
 
- function myFunction(x) {
-     x.classList.toggle("change");
- }
