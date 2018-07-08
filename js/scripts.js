62b74cbc45e4f4faf0d35c03d83e779c1dfbea94
fernneil10 <38779988+fernneil10@users.noreply.github.com>
2018-07-08 15:28:59 +0800

    Update scripts.js
    change script

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
  jQuery(document).ready(function($) {
-  
-  jQuery(window).load(function() {
-  
- jQuery('#fullpage').fullpage({
+
+     jQuery('#fullpage').fullpage({
          anchors: ['Home', 'AboutMe', 'MySkills', 'Portfolio', 'Contact'],
          menu: '#menu',
          navigation: true,
@@ -12,24 +10,6 @@
          scrollBar: false,
          css3:false
      });
- 
-});
-
-
-jQuery(window).resize(function(){
- 
- jQuery('#fullpage').fullpage({
-         anchors: ['Home', 'AboutMe', 'MySkills', 'Portfolio', 'Contact'],
-         menu: '#menu',
-         navigation: true,
-         autoScrolling: true,
-         parallax: true,
-         parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-         scrollBar: false,
-         css3:false
-     });
- 
-});
   
   jQuery('.portfolio-slider').slick({
          centerMode: true,
@@ -89,9 +69,70 @@ jQuery(window).resize(function(){
          ]
      });
 
-     
+
+    equalheight = function(container){
+
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
+ $(container).each(function() {
+
+   $el = $(this);
+   $($el).height('auto')
+   topPostion = $el.position().top;
+
+   if (currentRowStart != topPostion) {
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+   } else {
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+  }
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+ });
+}
+
+$(window).load(function() {
+  equalheight('.eq-blocks');
+});
 
 
+$(window).resize(function(){
+  equalheight('.eq-blocks');
+});
+
+
+  // fc slider
+  jQuery('.testimonials-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    // fade: true,
+    // cssEase: 'linear',
+    arrows: true,
+    slidesToShow: 2,
+    autoplay: true,
+      autoplaySpeed: 5000,
+      responsive: [
+
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+    ]
+  });
 
      jQuery('.burger-menu').click(function() {
          jQuery('.header-fix-menu').toggleClass('open-menu');
@@ -99,7 +140,6 @@ jQuery(window).resize(function(){
 
  });
 
-function myFunction(x) {
+ function myFunction(x) {
      x.classList.toggle("change");
  }
-
